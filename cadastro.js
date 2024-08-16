11163213d14fc1d2ea0f93e10b6accc75cd0db37
Thanzito <163420206+Thanzito@users.noreply.github.com>
2024-08-16 12:12:37 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,9 @@
+console.log("Cadastro do cliente");
+
+const nome = "Nathan Cavalcante";
+const idade = 16;
+const email = "nathancava679@gmail.com";
+const endereco = "Rua doutor José Alexandre de Moura Negrini";
+const profissao = "Estudante";
+
+console.log(`Bom dia ${nome}, você tem ${idade} anos, seu email é ${email}, seu endereço é ${endereco}, e sua profissão é ${profissao}`);
